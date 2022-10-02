8c0a105383595ea6aa059659c13047549f4db9eb
areee <10089872+areee@users.noreply.github.com>
2022-10-02 16:55:28 +0300

    Update kassakuitti_test tests

--- test/kassakuitti_test.dart
+++ test/kassakuitti_test.dart
@@ -1,86 +1,39 @@
 import 'package:kassakuitti/kassakuitti.dart';
+import 'package:kassakuitti/src/utils/selected_file_format_helper.dart';
+import 'package:kassakuitti/src/utils/selected_shop_helper.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // group('A group of tests', () {
-  //   final awesome = Awesome();
-
-  //   setUp(() {
-  //     // Additional setup goes here.
-  //   });
-
-  //   test('First Test', () {
-  //     expect(awesome.isAwesome, isTrue);
-  //   });
-  // });
-
   group('Kassakuitti', () {
-    // TODO: Update tests
-    // final kassakuitti = Kassakuitti();
+    final kassakuitti = Kassakuitti(
+        textFilePath: 'test.txt',
+        htmlFilePath: 'test.html',
+        selectedShop: SelectedShop.kRuoka,
+        selectedFileFormat: SelectedFileFormat.csv);
 
     setUp(() {
       // Additional setup goes here.
     });
+    test('Kassakuitti constructor', () {
+      expect(kassakuitti.textFilePath, 'test.txt');
+      expect(kassakuitti.htmlFilePath, 'test.html');
+      expect(kassakuitti.selectedShop, SelectedShop.kRuoka);
+      expect(kassakuitti.selectedFileFormat, SelectedFileFormat.csv);
+    });
 
-    // test('First Test', () {
-    //   expect(kassakuitti.textFilePath, isNull);
-    // });
+    test('Kassakuitti toString', () {
+      expect(kassakuitti.toString(),
+          'Kassakuitti(textFilePath: test.txt, htmlFilePath: test.html, selectedShop: SelectedShop.kRuoka, selectedFileFormat: SelectedFileFormat.csv)');
+    });
 
-    // test('Second Test', () {
-    //   expect(kassakuitti.htmlFilePath, isNull);
-    // });
-
-    // test('Third Test', () {
-    //   expect(kassakuitti.selectedShop, isNull);
-    // });
-
-    // test('Fourth Test', () {
-    //   expect(kassakuitti.selectedFileFormat, isNull);
-    // });
-
-    // test('Fifth Test', () {
-    //   expect(kassakuitti.toString(),
-    //       'Kassakuitti(textFilePath: null, htmlFilePath: null, selectedShop: null, selectedFileFormat: null)');
-    // });
-
-    // test('Sixth Test', () {
-    //   kassakuitti.textFilePath = 'test.txt';
-    //   expect(kassakuitti.textFilePath, 'test.txt');
-    // });
-
-    // test('Seventh Test', () {
-    //   kassakuitti.htmlFilePath = 'test.html';
-    //   expect(kassakuitti.htmlFilePath, 'test.html');
-    // });
-
-    // test('Eighth Test', () {
-    //   kassakuitti.selectedShop = 'test';
-    //   expect(kassakuitti.selectedShop, 'test');
-    // });
-
-    // test('Ninth Test', () {
-    //   kassakuitti.selectedFileFormat = 'test';
-    //   expect(kassakuitti.selectedFileFormat, 'test');
-    // });
-
-    // test('Tenth Test', () {
-    //   kassakuitti.textFilePath = 'test.txt';
-    //   kassakuitti.htmlFilePath = 'test.html';
-    //   kassakuitti.selectedShop = 'test';
-    //   kassakuitti.selectedFileFormat = 'test';
-    //   expect(kassakuitti.toString(),
-    //       'Kassakuitti(textFilePath: test.txt, htmlFilePath: test.html, selectedShop: test, selectedFileFormat: test)');
-    // });
-
-    // test('Eleventh Test', () {
-    //   kassakuitti.textFilePath = 'test.txt';
-    //   kassakuitti.htmlFilePath = 'test.html';
-    //   kassakuitti.selectedShop = 'test';
-    //   kassakuitti.selectedFileFormat = 'test';
-    //   expect(kassakuitti.textFilePath, 'test.txt');
-    //   expect(kassakuitti.htmlFilePath, 'test.html');
-    //   expect(kassakuitti.selectedShop, 'test');
-    //   expect(kassakuitti.selectedFileFormat, 'test');
-    // });
+    test('Kassakuitti default values', () {
+      final kassakuittiDefaultValues =
+          Kassakuitti(textFilePath: 'test.txt', htmlFilePath: 'test.html');
+      expect(kassakuittiDefaultValues.textFilePath, 'test.txt');
+      expect(kassakuittiDefaultValues.htmlFilePath, 'test.html');
+      expect(kassakuittiDefaultValues.selectedShop, SelectedShop.sKaupat);
+      expect(kassakuittiDefaultValues.selectedFileFormat,
+          SelectedFileFormat.excel);
+    });
   });
 }
